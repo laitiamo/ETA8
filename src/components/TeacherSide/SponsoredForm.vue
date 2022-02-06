6d0768c338d67caeeb29a4a76e3d6f5de4573c20
toma <54888296+laitiamo@users.noreply.github.com>
2022-02-06 19:54:12 +0800

    前端完工

--- src/components/TeacherSide/SponsoredForm.vue
+++ src/components/TeacherSide/SponsoredForm.vue
@@ -1176,25 +1176,75 @@ export default {
           let data2upload = new FormData();
           //获取实际input组件的文件
           let filesList = this.FormData.subjectFileList;
+          //上传项目基础信息（项目申报）
+          data2upload.append("subjectName", this.FormData.subjectName);
           data2upload.append("subjectNum", this.FormData.subjectNum);
           data2upload.append("subjectType", this.RankName);
-          data2upload.append("subjectName", this.FormData.subjectName);
-          data2upload.append("subjectTime", this.FormData.subjectTime);
-          data2upload.append("startTime", this.FormData.startTime);
-          data2upload.append("FinishTime", this.FormData.FinishTime);
-          data2upload.append("subjectPlace", this.FormData.subjectPlace);
           data2upload.append("rankId", this.FormData.rankId);
-          data2upload.append("levelId", this.levelId);
-          data2upload.append("subjectFund", this.FormData.subjectFund);
+          data2upload.append("levelId", this.levelId);  //项目的等级id（横向纵向）
           for (let i = 0; i < this.FormData.domains.length; i++) {
             data2upload.append("userids[]", this.FormData.domains[i].value);
           }
-          //校级项目表单
-          data2upload.append("EconomicId", this.FormData.EconomicId);
+
+          //项目经费
+          data2upload.append("subjectFund", this.FormData.subjectFund);
+          data2upload.append("hardwareFund", this.FormData.hardwareFund);
+          data2upload.append("softwareFund", this.FormData.softwareFund);
+          data2upload.append("staySchoolFund", this.FormData.staySchoolFund);
+          data2upload.append("outboundFund", this.FormData.outboundFund);
+
+          //项目描述
+          data2upload.append("subjectPlace", this.FormData.subjectPlace);
+          data2upload.append("ResearchId", this.FormData.ResearchId);
+          data2upload.append("isSecrecy", this.FormData.isSecrecy);
+          data2upload.append("isVoucher", this.FormData.isVoucher);
+          data2upload.append("isSubmitFill", this.FormData.isSubmitFill);
+          data2upload.append("isPromote", this.FormData.isPromote);
+          data2upload.append("isDutyFree", this.FormData.isDutyFree);
+          data2upload.append("DutyFreeId", this.FormData.DutyFreeId);
+          data2upload.append("subjectTime", this.FormData.subjectTime);
+          data2upload.append("startTime", this.FormData.startTime);
+          data2upload.append("FinishTime", this.FormData.FinishTime);
+
+          //教育部统计信息
+          data2upload.append("BelongId", this.FormData.BelongId);
+          data2upload.append("TypeId", this.FormData.TypeId);
+          data2upload.append("entrustPlaceId", this.FormData.entrustPlaceId);
+          data2upload.append("topicId", this.FormData.topicId);
+          data2upload.append("mainProjectName", this.FormData.mainProjectName);
+          data2upload.append("applicationCode", this.FormData.applicationCode);
+          data2upload.append("mainProjectName", this.FormData.mainProjectName);
+          data2upload.append("introduction", this.FormData.introduction);
+
+          //合作单位
+          data2upload.append("remarks", this.FormData.remarks);
+          data2upload.append("cooperateId", this.FormData.cooperateId);
+          data2upload.append("contractId", this.FormData.contractId);
+          data2upload.append("fundId", this.FormData.fundId);
+          data2upload.append("contractName", this.FormData.contractName);
+          data2upload.append("contractType", this.FormData.contractType);
+          data2upload.append("contractFund", this.FormData.contractFund);
+          data2upload.append(
+            "cooperatePrincipal",
+            this.FormData.cooperatePrincipal
+          );
+          data2upload.append("bankName", this.FormData.bankName);
+          data2upload.append("bankAccount", this.FormData.bankAccount);
+          data2upload.append(
+            "dutyBreachContract",
+            this.FormData.dutyBreachContract
+          );
+
+          //技术市场信息
+          data2upload.append("payId", this.payId);
+          data2upload.append("EcoFirstId", this.EcoFirstId);
+          data2upload.append("EcoSecondId", this.EcoSecondId);
+          data2upload.append("EconomicId", this.EconomicId);
+          data2upload.append("SocFirstId", this.SocFirstId);
           data2upload.append("SocietyId", this.FormData.SocietyId);
           data2upload.append("SourceId", this.FormData.SourceId);
-          data2upload.append("typeId", this.FormData.TypeId);
-          data2upload.append("cooperateId", this.FormData.cooperateId);
+          data2upload.append("TechnicalTypeId", this.TechnicalTypeId);
+          data2upload.append("PropertyId", this.PropertyId);
 
           //循环加入多文件
           for (let i = 0; i < filesList.length; i++) {
@@ -1250,6 +1300,7 @@ export default {
         }
       });
     },
+
     //取消上传，清空文件列表及表单
     cancelUpload(formName) {
       this.$refs[formName].resetFields();
